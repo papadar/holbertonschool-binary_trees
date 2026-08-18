5b0c94fcd956037efb415b31f01945deacae8480
papadar <aladar@apponyi.net>
2026-08-18 01:15:20 +0000

    program to add a left node

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,40 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_left - adds a node to the 'left' branch of a parent
+ * @parent: the parent node to attach to
+ * @value: the data to store
+ * Return: a pointer to the new node
+ */
+
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *node = malloc(sizeof(binary_tree_t));
+	binary_tree_t *spot = parent->left;
+
+	if (!node)
+	{
+		printf("didnt make a node - returning null");
+		return (NULL);
+	}
+	if (!parent)
+	{
+		printf("didnt find a parent - returning null");
+		return (NULL);
+	}
+	node->n = value;
+	node->right = NULL;
+	node->parent = parent;
+	parent->left = node;
+
+	if (spot == NULL)
+	{
+		node->left = NULL;
+	}
+	else
+	{
+		node->left = spot;
+		spot->parent = node;
+	}
+	return (node);
+}
