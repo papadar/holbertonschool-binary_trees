203b91fc33546e40733c0ffe3db574312c6ca781
papadar <aladar@apponyi.net>
2026-08-18 01:24:17 +0000

    adding free of node if parent not found

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -15,8 +15,10 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (!node)
 		return (NULL);
 	if (!parent)
+	{
+		free(node);
 		return (NULL);
-
+	}
 	spot = parent->left;
 	node->n = value;
 	node->right = NULL;
